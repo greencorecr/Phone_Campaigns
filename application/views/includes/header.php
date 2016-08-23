5f705ca7e313955be07775efaa2c1165a44a56cb
jcgut <jcga_1994@hotmail.com>
2016-08-23 13:15:31 -0600

    incorporación de librerías para ciertas funcionalidades a nivel de interfaz gráfica

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,9 +4,24 @@
   <title>{_phone_campaigns} :: <?=$page_title?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
+<!-- Boostrap -->
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap/css/bootstrap.min.css" media="screen">
 <script src="<?php echo base_url(); ?>assets/css/bootstrap/js/jquery.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/css/bootstrap/js/bootstrap.min.js"></script>
+
+
+<!-- Para funciones de modales y manipulacion de componentes-->
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/basic.css" type="text/css" media="screen" />
+<script src="<?php echo base_url(); ?>assets/js/jquery-1.7.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.tools.min.js"></script>
+
+<!-- Para el datapicker -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap/css/datepicker.min.css" /> 
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap/css/datepicker3.min.css" />
+<script src="<?php echo base_url(); ?>assets/css/bootstrap/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/dateinput.css" type="text/css" media="screen" /> 
+
+
 
 
   <link rel="shortcut icon" href="<?php echo base_url(); ?>favicon.ico" />
