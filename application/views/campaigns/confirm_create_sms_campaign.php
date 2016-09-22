7941ee2dad13857296197eefeacd4022f5b9b6c0
jcgut <jcga_1994@hotmail.com>
2016-09-22 15:01:11 -0600

    Inclusión de interfaz gráfica para confirmar las campañas SMS programadas

--- application/views/campaigns/confirm_create_sms_campaign.php
+++ application/views/campaigns/confirm_create_sms_campaign.php
@@ -1,59 +1,86 @@
 <?php //echo "<pre>".print_r($_POST, true)."</pre>"; ?>
-<div id="container">
-    <h2>{_title_new_sms_campaign}</h2>
+
+</br></br></br></br>
+<div id="container" class="container">
+
+    <ol class="breadcrumb">
+    <li><a href="<?=base_url()?>">{_menu_home}</a></li>
+    <li><a href="<?=base_url()?>campaigns">{_menu_campaigns}</a></li>
+    <li class="active">{_title_new_campaign}</li>
+    </ol>
+
+    <div class="jumbotron">  
+    <div class="panel panel-default">
+    <div class="panel-body">
+
      <div class="main_content">
+
         <div id="campaign_form">
         <?=validation_errors();?>
         <?php if(isset($errors)) echo  "<div class='error'>$errors</div>"; ?>
 
         <?=form_open('campaigns/finalize_create_sms_campaign');?>
-        <div class="div_labels"><?=form_label('{_campaign_label_name}: ', 'name');?></div>
-        <div class="div_inputs"><?=$post_data['name']?></div><div class="clearfix"></div>
-        <div class="div_labels"><?=form_label('{_campaign_label_start}: ', 'date_start');?></div>
-        <div class="div_inputs"><?=$post_data['date_start']?></div><div class="clearfix"></div>
-        <div class="div_labels"><?=form_label('{_campaign_label_end}: ', 'date_end');?></div>
-        <div class="div_inputs"><?=$post_data['date_end']?></div><div class="clearfix"></div>
-        <div class="div_labels"><?=form_label('{_campaign_label_hour_start}: ', 'day_start_hour');?></div>
-        <div class="div_inputs"><?=$post_data['day_start_hour']?>:<?=$post_data['day_start_min']?></div><div class="clearfix"></div>
-        <div class="div_labels"><?=form_label('{_campaign_label_hour_end}: ', 'daytime_hour');?></div>
-        <div class="div_inputs"><?=$post_data['day_end_hour']?>:<?=$post_data['day_end_min']?></div><div class="clearfix"></div>
 
-        <div class="div_labels"><?=form_label('{_campaign_label_sms_message}: ', 'sms_message');?></div>
-        <div class="div_inputs"><?=$post_data['sms_message']?></div><div class="clearfix"></div>
 
-        <div class="div_labels"><?=form_label('{_campaign_label_file}: ', 'userfile');?></div>
-        <div class="div_inputs"><?=$upload_data['file_name']?>
+        <div class="panel panel-primary">
+        <div class="panel-heading"><center>{_title_new_sms_campaign}</center></div>
+        <div class="panel-body">
 
-            <br /> - {_total_rows}: <?=$summary['total_rows']?>
-            <br /> - {_valid_rows}: <?=$summary['valid_rows']?>
-            <br /><a target="_blank" href="<?=base_url()?>upload/temp/upload_data.txt">{_data}</a>
+        <div class="row">
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_name}: ', 'name');?> <?=$post_data['name']?></div>
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_start} de la Campaña: ', 'date_start');?> <?=$post_data['date_start']?></div>
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_end} de la Campaña: ', 'date_end');?> <?=$post_data['date_end']?></div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12" style="background-color:#d9edf7;"> <br /></div>
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_hour_start}: ', 'day_start_hour');?> <?=$post_data['day_start_hour']?>:<?=$post_data['day_start_min']?></div>
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_hour_end}: ', 'daytime_hour');?> <?=$post_data['day_end_hour']?>:<?=$post_data['day_end_min']?></div>
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_file}: ', 'userfile');?> <?=$upload_data['file_name']?></div>
+        </div>
 
-            <?php if(!empty($summary['errors'])): ?>
-            <br /><a target="_blank" href="<?=base_url()?>upload/temp/upload_errors.txt">{_errors}</a>
-            <?php endif; ?>
+        <div class="row">
+            <div class="col-xs-12" style="background-color:#d9edf7;"> <br /></div>
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <label>{_total_rows}:</label> <?=$summary['total_rows']?> </div>
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <label>{_valid_rows}:</label> <?=$summary['valid_rows']?> </div>     
+            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_priority}: ', 'priority');?> <?=$post_data['priority']?></div>           
+        </div>
+     
+        <div class="row">
+            <div class="col-xs-12" style="background-color:#d9edf7;"> <br /></div>
+            <center><div class="col-xs-12 col-md-12" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_sms_message}: ', 'sms_message');?></div></center>
+            <center><div class="col-xs-12 col-md-12" style="background-color:#d9edf7"><?=$post_data['sms_message']?></div></center>  
+            <div class="col-xs-12" style="background-color:#d9edf7;"> <br /><br /></div> 
+        </div></br>
 
-        </div><div class="clearfix"></div>
+        <div class="row">
+            <a target="_blank" class="btn btn-info btn-xs" role="button" href="<?=base_url()?>upload/temp/upload_data.txt">{_data}</a>
+             <?php if(!empty($summary['errors'])): ?>
+                 <a target="_blank" class="btn btn-danger btn-xs" role="button" href="<?=base_url()?>upload/temp/upload_errors.txt">{_errors}</a>      
+        <?php endif; ?>
+        </div>
+ 
         <?php if($summary['valid_rows'] > $calls_insert_batch_size): ?>
-            <div class="div_labels">&nbsp;</div>
-            <div class="div_inputs"><?='{_split_campaign_warning}'?></div><div class="clearfix"></div>
-        <?php endif; ?>                
+            <div class="row">
+                <div style="color: red;"><?='{_split_campaign_warning}'?></div>
+            </div>
+        <?php endif; ?>   
         
-        <div class="div_labels"><?=form_label('{_campaign_label_priority}: ', 'priority');?></div>
-        <div class="div_inputs"><?=$post_data['priority']?></div><div class="clearfix"></div>
-        
-        <div class="div_labels">&nbsp;</div>
+        </div>
+       </div>
+
         <div class="div_inputs">
-            <?=form_button(array('name' => 'confirm_and_run', 'id' => 'confirm_and_run', 'content' => '{_button_confirm_and_start}'));?>
-            <?=form_submit(array('name' => 'confirm', 'id' => 'confirm', 'value' => '{_button_confirm}'));?>
-            <?=form_button(array('name' => 'cancel', 'id' => 'cancel', 'content' => '{_button_cancel}'));?>
+            <?=form_button(array('name' => 'confirm_and_run', 'class' => 'btn btn-success', 'id' => 'confirm_and_run', 'content' => '{_button_confirm_and_start}'));?>
+            <?=form_submit(array('name' => 'confirm', 'class' => 'btn btn-info', 'id' => 'confirm', 'value' => '{_button_confirm}'));?>
+            <?=form_button(array('name' => 'cancel', 'class' => 'btn btn-warning', 'id' => 'cancel', 'content' => '{_button_cancel}'));?>
         </div><div class="clearfix"></div>
 
         <?=form_close();?>
         </div>
-
+       </div>
+      </div>
      </div>
-
-</div>
+    </div>
+   </div>
 
 <script type="text/javascript">
 
