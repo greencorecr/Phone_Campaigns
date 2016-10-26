fc43c377b034fecb030ad19cfe0f27688e599c2e
jcgut <jcga_1994@hotmail.com>
2016-10-26 13:57:48 -0600

    inclusión de interfaz gráfica para confirmar las campañas telefónicas programadas

--- application/views/campaigns/confirm_create_campaign.php
+++ application/views/campaigns/confirm_create_campaign.php
@@ -2,6 +2,7 @@
 </br></br></br></br>
 <div id="container" class="container">
 
+
     <ol class="breadcrumb">
     <li><a href="<?=base_url()?>">{_menu_home}</a></li>
     <li><a href="<?=base_url()?>campaigns">{_menu_campaigns}</a></li>
@@ -19,41 +20,45 @@
 
         <?=form_open('campaigns/finalize_create_campaign');?>
 
-        <div class="panel panel-primary">
-        <div class="panel-heading"><center>{_title_new_campaign} Telefónica</center></div>
+        <div class="panel">
         <div class="panel-body">
 
-        <div class="row">
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_name}: ', 'name');?> <?=$post_data['name']?></div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_start} de la Campaña: ', 'date_start');?> <?=$post_data['date_start']?></div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_end} de la Campaña: ', 'date_end');?> <?=$post_data['date_end']?></div>
+        <!-- Confirmacion de la creacion de la campanna telefonica -->
+
+        <div class="row" style="background-color: #DFF0D8;">
+        <div class="col-xs-12" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;">
+            <center><label>{_title_new_campaign}</label>
         </div>
-        <div class="row">
-            <div class="col-xs-12" style="background-color:#d9edf7;"> <br /></div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_hour_start}: ', 'day_start_hour');?> <?=$post_data['day_start_hour']?>:<?=$post_data['day_start_min']?></div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_hour_end}: ', 'daytime_hour');?> <?=$post_data['day_end_hour']?>:<?=$post_data['day_end_min']?></div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_file}: ', 'userfile');?> <?=$upload_data['file_name']?></div>
+        </div></br></br>
+        <div class="row" style="background-color: #DFF0D8;">
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"><?=form_label('{_campaign_label_name}: ', 'name');?> <?=$post_data['name']?></div>
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"><?=form_label('{_campaign_label_start} de la Campaña: ', 'date_start');?> <?=$post_data['date_start']?></div>
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"><?=form_label('{_campaign_label_end} de la Campaña: ', 'date_end');?> <?=$post_data['date_end']?></div>
         </div>
-        <div class="row">
-            <div class="col-xs-12" style="background-color:#d9edf7;"> <br /></div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <label>{_total_rows}:</label> <?=$summary['total_rows']?> </div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <label>{_valid_rows}:</label> <?=$summary['valid_rows']?> </div>     
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"><?=form_label('{_campaign_label_priority}: ', 'priority');?> <?=$post_data['priority']?></div>           
+        <div class="row" style="background-color: #DFF0D8;">
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <?=form_label('{_campaign_label_hour_start}: ', 'day_start_hour');?> <?=$post_data['day_start_hour']?>:<?=$post_data['day_start_min']?></div>
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <?=form_label('{_campaign_label_hour_end}: ', 'daytime_hour');?> <?=$post_data['day_end_hour']?>:<?=$post_data['day_end_min']?></div>
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"><?=form_label('{_campaign_label_file}: ', 'userfile');?> <?=$upload_data['file_name']?></div>
         </div>
-        <div class="row">
-            <div class="col-xs-12" style="background-color:#d9edf7;"> <br /></div>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_retries}: ', 'retries');?> <?=$post_data['retries']?></div>
+        <div class="row" style="background-color: #DFF0D8;">
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <label>{_total_rows}:</label> <?=$summary['total_rows']?> </div>
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <label>{_valid_rows}:</label> <?=$summary['valid_rows']?> </div>     
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"><?=form_label('{_campaign_label_priority}: ', 'priority');?> <?=$post_data['priority']?></div>           
+        </div>
+        <div class="row" style="background-color: #DFF0D8;">
+            
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <?=form_label('{_campaign_label_retries}: ', 'retries');?> <?=$post_data['retries']?></div>
             <?php if($post_data['recording2'] == null): ?>
-            <div class="col-xs-6 col-md-8" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_recording}: ', 'recording');?> <?=$post_data['recording']?></div>     
+            <div class="col-xs-6 col-md-8" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <?=form_label('{_campaign_label_recording}: ', 'recording');?> <?=$post_data['recording']?></div>     
             <?php endif; ?>
             <?php if($post_data['recording2'] != null): ?>
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_recording}: ', 'recording');?> <?=$post_data['recording']?></div>     
-            <div class="col-xs-6 col-md-4" style="background-color:#99c4f4;"> <?=form_label('{_campaign_label_recording2}: ', 'recording2');?> <?=$post_data['recording2']?></div>
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <?=form_label('{_campaign_label_recording}: ', 'recording');?> <?=$post_data['recording']?></div>     
+            <div class="col-xs-6 col-md-4" style="padding: 0.5%; border-width: 0.1px; border-color: #bbcfca; border-style: solid;"> <?=form_label('{_campaign_label_recording2}: ', 'recording2');?> <?=$post_data['recording2']?></div>
             <?php endif; ?>
         </div><br/>
 
         <div class="row">
-            <a target="_blank" class="btn btn-info btn-xs" role="button" href="<?=base_url()?>upload/temp/upload_data.txt">{_data}</a>
+            <a target="_blank" class="btn btn-primary btn-xs" role="button" href="<?=base_url()?>upload/temp/upload_data.txt">{_data}</a>
              <?php if(!empty($summary['errors'])): ?>
                  <a target="_blank" class="btn btn-danger btn-xs" role="button" href="<?=base_url()?>upload/temp/upload_errors.txt">{_errors}</a>      
         <?php endif; ?>
@@ -69,10 +74,10 @@
        </div>   
    
         <div class="div_labels">&nbsp;</div>
-        <div class="div_inputs">
+        <div class="div_inputs" style="padding:%;">
             <?=form_button(array('name' => 'confirm_and_run','class' => 'btn btn-success', 'id' => 'confirm_and_run', 'content' => '{_button_confirm_and_start}'));?>
-            <?=form_submit(array('name' => 'confirm', 'class' => 'btn btn-info','id' => 'confirm', 'value' => '{_button_confirm}'));?>
-            <?=form_button(array('name' => 'cancel', 'class' => 'btn btn-warning', 'id' => 'cancel', 'content' => '{_button_cancel}'));?>
+            <?=form_submit(array('name' => 'confirm', 'class' => 'btn btn-primary','id' => 'confirm', 'value' => '{_button_confirm}'));?>
+            <?=form_button(array('name' => 'cancel', 'class' => 'btn btn-danger', 'id' => 'cancel', 'content' => '{_button_cancel}'));?>
         </div><div class="clearfix"></div>
 
         <?=form_close();?>
@@ -89,7 +94,8 @@
 
     $(document).ready(function(){
 
-        $("#confirm_and_run").click(function(){
+        /* Redirecciones de vistas dependiendo de si es o no creada la campanna */
+        $("#confirm_and_run").click(function(){ 
             window.location = base_url + 'campaigns/finalize_create_campaign/true';
         });
         $("#cancel").click(function(){
