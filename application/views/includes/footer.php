6f2c06fad095040382419ebc29ba5f4ca1e359c4
jcgut <jcga_1994@hotmail.com>
2016-10-26 14:32:50 -0600

    reajuste en el footer (interfaz)

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,7 +1,7 @@
 
 
 
-<center><div style=" background:#dbe3e1; padding:3px;" class="footer navbar-fixed-bottom"><div class="container"><p class="text-muted">Campañas Telefónicas | © 2016 
+<center><div style="background:#dbe3e1;" class="footer navbar-fixed-bottom"><div class="container"><p class="text-muted" style="padding-top: 5px;">Campañas Telefónicas | © 2016 
 Derechos Reservados </p></div></div></center>
 
 </body>
