119a773978592d1ae30b506adbe3d939121314b4
jcgut <jcga_1994@hotmail.com>
2016-10-26 14:20:18 -0600

    cambio de interfaz gráfica para el envío de SMS de test y así poder montarlo en una ventana emergente

--- application/views/campaigns/send_sms.php
+++ application/views/campaigns/send_sms.php
@@ -1,5 +1,11 @@
 <div id="container">
-    
+
+<!-- form para el envio de mensajes de prueba  -->
+<div class="modal-header">          
+        <div id="header" class="alert alert-info">
+        <strong id="strong">¡{_title_send_test_sms}!</strong></div>
+</div>
+
      <div class="main_content" style='width: 90%; margin: auto;' >
         <div id="campaign_sms_form">
             <div><h4><?=form_label('{_label_phone_number} ', 'phone_number');?></h4>
@@ -26,25 +32,32 @@
                          'name' => 'message'
                         ,'id' => 'message'
                         ,'class' => 'form-control'
-                        ,'rows' => '5'
+                        ,'rows' => '1'
                         ,'cols' => '50'
                         ,'value' => set_value('message', $sms_test_message)));?></div>
-                        <h5>*{_sms_message_amount}</h5><div class="clearfix"></div>
+                        <h5>- *{_sms_message_amount}</h5>
+                        <h5>- ¡Colocar 3 signos de dolar ($$$) donde desea establecer el monto!</h5>
+                        <strong id="send" style="color: green; display: none">¡Se envió el mensaje de prueba, sal o envía otro de nuevo!</strong><div class="clearfix"></div>
             <div><label></label><h4><div id="errorMessage"></div></h4><div class="clearfix"></div>
             <div><label></label>
-            <br />
+            
             <?=form_submit(array('name' => 'submit','id' => 'submit', 'class' => 'btn btn-success btn-sm', 'style' => 'width: 185px;', 'value' => '{_button_submit}'));?></div><div class="clearfix"></div>
             <label></label><div id="results"></div></div><div class="clearfix"></div>
 
         </div>
     </div>
+
 </div>
+
 
 <script type="text/javascript" language="javascript" src="<?php echo base_url(); ?>assets/js/jquery.simplemodal.js"></script>
 
 <script type="text/javascript">
+ var x = 0; // variable para cambiar el color de amarillo a verde y viceversa
 
+     /* valida errores en datos suministrados por el usuario , de no ser asi envia el mensaje mediante ajax*/
     $(document).ready(function() {
+       
 
         $(".footer").css("display", "none");
         
@@ -65,12 +78,12 @@
             var valid = true;
 
             if(!eightDigits.test(phoneNumber)){
-                $("#errorMessage").append("must be a valid phone number.<br />");
+                $("#errorMessage").append("Debe ser un número de teléfono válido.<br />");
                 valid = false;
             }
 
             if(!oneSixtyChars.test(message)){
-                $("#errorMessage").append("message must be between 1 and 160 in length.<br />");
+                $("#errorMessage").append("Mensaje debe contener entre 1 y 160 caracteres.<br />");
                 valid = false;
             }
 
@@ -84,7 +97,22 @@
                 function(data){
                     $('#results').html('{_message sent}!<br />' + data.phone + "<br />" + data.msg);
             }, "json");
-            
+
+          
+          // disenno sms test
+          
+          $("#strong").empty();
+          $("#strong").text("¡Mensaje Enviado Correctamente!"); 
+          
+          
+          if(x == 0){$("#header").removeClass( "alert alert-info" ).addClass( "alert alert-success" ); x = 1; $("#send").css("color", "green"); }
+          
+          else{ if(x == 1) {$("#header").removeClass( "alert alert-success" ).addClass( "alert alert-info" ); x = 0; $("#send").css("color", "blue");} }
+          
+
+          $("#send").css("display", "block"); 
+
+         
         });
 
         $('#phone_number').keyup( function(event) {
@@ -100,5 +128,6 @@
         });
 
     });
+
 
 </script>
